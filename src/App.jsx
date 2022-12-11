c862e7893d3b59f017dc1f2e466962d349da9748
GeekchanskiY <astrovskiy.04@gmail.com>
2022-12-12 00:29:37 +0300

    basement ready

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="main_header">
+      <div className='app-title'>Notes</div>
+      <div className="user">
+          <a href="">UserName</a>
+        </div>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
